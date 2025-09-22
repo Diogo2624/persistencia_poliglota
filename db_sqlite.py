2e8b0f3c42c664feb05d020213fa6878750bac45
Diogo Henrique <henriquepanazzolo2018@hotmail.com>
2025-09-22 20:23:14 -0300

    Finalizando db_sqlite

--- db_sqlite.py
+++ db_sqlite.py
@@ -14,3 +14,18 @@ def criar_tabela():
     """)
     conn.commit()
     conn.close()
+
+def inserir_cidade(nome, estado):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO cidades (nome, estado) VALUES (?, ?)", (nome, estado))
+    conn.commit()
+    conn.close()
+
+def listar_cidades():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM cidades")
+    cidades = cursor.fetchall()
+    conn.close()
+    return cidades
