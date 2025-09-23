7f45e8ed58c749ea14df34fdc26f6759838dba82
unknown <emelu.leme@gmail.com>
2025-09-22 21:38:14 -0300

    adicionando comandos para o geoprocessamento

---
+++ geoprocessamento.py
@@ -0,0 +1,14 @@
+from geopy.distance import geodesic
+
+def calcular_distancia(coord1, coord2):
+    return geodesic(coord1, coord2).km
+
+def locais_proximos(lista_locais, coord_ref, raio_km=10):
+    proximos = []
+    for local in lista_locais:
+        lat = local["coordenadas"]["latitude"]
+        lon = local["coordenadas"]["longitude"]
+        distancia = calcular_distancia(coord_ref, (lat, lon))
+        if distancia <= raio_km:
+            proximos.append((local["nome_local"], distancia))
+    return proximos
